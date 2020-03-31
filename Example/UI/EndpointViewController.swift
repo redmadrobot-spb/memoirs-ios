adb19db3e5601c684d9cf657e7aaed7d21b56eb9
Roman Mazeev <r.mazeev@redmadrobot.com>
2020-03-31 16:47:38 +0300

    Switch to code display instead of getting

--- Example/UI/EndpointViewController.swift
+++ Example/UI/EndpointViewController.swift
@@ -10,96 +10,47 @@ import UIKit
 import Robologs
 
 class EndpointViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet private var doneBarButtonItem: UIBarButtonItem!
     @IBOutlet private var connectionUrlTextField: UITextField!
-    @IBOutlet private var connectionCodeTextField: UITextField!
     @IBOutlet private var secretTextField: UITextField!
     @IBOutlet private var formStackView: UIStackView!
     @IBOutlet private var formBottomConstraint: NSLayoutConstraint!
-    private var loadingView: UIView!
+    @IBOutlet private var connectButton: ActionButton!
+    @IBOutlet private var codeStackViewCenterConstraint: NSLayoutConstraint!
+    @IBOutlet private var codeStackView: UIStackView!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
 
     private var isLoading = false {
         didSet {
-            loadingView.isHidden = !isLoading
-            doneBarButtonItem.isEnabled = !isLoading
+            activityIndicator.isHidden = !isLoading
+            connectButton.isEnabled = !isLoading
+        }
+    }
+
+    private var isConnected = false {
+        didSet {
+            codeStackView.isHidden = !isConnected
+            connectButton.setTitle(isConnected ? "Disconnect" : "Connect", for: .normal)
+            connectButton.backgroundColor = isConnected ? .systemRed : .systemBlue
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupLoadingView()
         setupKeyboardShowing()
-        addDoneButtonOnConnectionCodeKeyboard()
+        codeStackView.isHidden = true
     }
 
     @IBAction func cancelButtonTapped() {
         self.dismiss(animated: true)
     }
 
-    @IBAction func doneButtonTapped() {
-        isLoading = true
-
-        guard let urlString = connectionUrlTextField.text, let url = URL(string: urlString) else {
-            showErrorAlert()
-            return
-        }
-
-        let transport = ProtoHttpRemoteLoggerTransport(endpoint: url, secret: secretTextField.text ?? "")
-        transport.authorize { result in
-
-            if case .failure = result {
-                self.showErrorAlert()
-            } else {
-                RemoteLoggerService.logger = RemoteLogger(buffering: InMemoryBuffering(), transport: transport)
-                transport.startLiveSession(self.connectionCodeTextField.text ?? "")
-                self.isLoading = false
-                self.dismiss(animated: true)
-            }
-        }
-    }
-
     private func showErrorAlert() {
         self.isLoading = false
+        self.isConnected = false
         let alert = UIAlertController(title: "Can not connect", message: nil, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .cancel))
         present(alert, animated: true)
-    }
-
-    private func setupLoadingView() {
-        let loadingView = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
-        loadingView.backgroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.2)
-        loadingView.layer.cornerRadius = 20
-        let activityIndicator = UIActivityIndicatorView(style: .white)
-        activityIndicator.startAnimating()
-        activityIndicator.center = loadingView.center
-        loadingView.center = view.center
-        loadingView.addSubview(activityIndicator)
-        self.view.addSubview(loadingView)
-        loadingView.isHidden = !isLoading
-        self.loadingView = loadingView
-    }
-
-    private func addDoneButtonOnConnectionCodeKeyboard() {
-        let doneToolbar: UIToolbar = UIToolbar(
-            frame: CGRect(
-                x: 0,
-                y: 0,
-                width: UIScreen.main.bounds.width,
-                height: 50
-            )
-        )
-
-        doneToolbar.barStyle = .default
-
-        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let doneBarButtonItem = UIBarButtonItem(
-            title: "Next", style: .done, target: self, action: #selector(self.connectionCodeNextAction))
-
-        doneToolbar.items = [flexSpace, doneBarButtonItem]
-        doneToolbar.sizeToFit()
-
-        connectionCodeTextField.inputAccessoryView = doneToolbar
     }
 
     private func setupKeyboardShowing() {
@@ -115,11 +66,6 @@ class EndpointViewController: UIViewController, UITextFieldDelegate {
             selector: #selector(self.keyboardWillHide),
             name: UIResponder.keyboardWillHideNotification,
             object: nil)
-        formBottomConstraint.constant = (view.frame.height / 2) - (formStackView.frame.height / 2)
-    }
-
-    @objc private func connectionCodeNextAction() {
-        secretTextField.becomeFirstResponder()
     }
 
     @objc private func dismissKeyboard() {
@@ -132,7 +78,8 @@ class EndpointViewController: UIViewController, UITextFieldDelegate {
         }
 
         if formBottomConstraint.constant <= keyboardSize.height {
-            formBottomConstraint.constant = keyboardSize.height + 16
+            codeStackViewCenterConstraint.isActive = false
+            formBottomConstraint.constant = keyboardSize.height
 
             UIView.animate(withDuration: 0.5) {
                 self.view.layoutIfNeeded()
@@ -141,32 +88,48 @@ class EndpointViewController: UIViewController, UITextFieldDelegate {
     }
 
     @objc private func keyboardWillHide(notification: Notification) {
-        formBottomConstraint.constant = (view.frame.height / 2) - (formStackView.frame.height / 2)
+        formBottomConstraint.constant = 0
+        codeStackViewCenterConstraint.isActive = true
 
          UIView.animate(withDuration: 0.5) {
             self.view.layoutIfNeeded()
          }
     }
 
+    @IBAction func connectButtonTapped() {
+        isLoading = true
+
+        if isConnected {
+            // TODO: Disconnect
+        } else {
+            guard let urlString = connectionUrlTextField.text, let url = URL(string: urlString) else {
+                showErrorAlert()
+                return
+            }
+
+            let transport = ProtoHttpRemoteLoggerTransport(endpoint: url, secret: secretTextField.text ?? "")
+            transport.authorize { result in
+
+                if case .failure = result {
+                    self.showErrorAlert()
+                } else {
+                    RemoteLoggerService.logger = RemoteLogger(buffering: InMemoryBuffering(), transport: transport)
+                    self.isLoading = false
+                    self.isConnected = true
+                }
+            }
+        }
+    }
+
     // MARK: - UITextFieldDelegate
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == connectionUrlTextField {
-            connectionCodeTextField.becomeFirstResponder()
-            return true
+            secretTextField.becomeFirstResponder()
         } else if textField == secretTextField {
-            doneButtonTapped()
-            return true
-        } else {
-            return true
+            secretTextField.resignFirstResponder()
         }
-    }
 
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let text = textField.text, textField == connectionCodeTextField else { return true }
-
-        let compSepByCharInSet = string.components(separatedBy: CharacterSet.decimalDigits.inverted)
-        let numberFiltered = compSepByCharInSet.joined(separator: "")
-        return string == numberFiltered && text.count < 6
+        return true
     }
 }
