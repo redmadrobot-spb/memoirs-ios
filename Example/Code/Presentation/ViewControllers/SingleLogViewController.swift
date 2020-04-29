944a98ccd7559bf205e3713d35b7a9ed998daf91
Alex Babaev <alex@devpocket.com>
2020-04-29 23:04:39 +0300

    Added ability to generate random JSON parts and random XML parts

--- Example/Code/Presentation/ViewControllers/SingleLogViewController.swift
+++ Example/Code/Presentation/ViewControllers/SingleLogViewController.swift
@@ -93,7 +93,10 @@ class SingleLogViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction
     private func autoGenerateMessage() {
-        messageTextField.text = String(loremIpsum.prefix((0 ..< loremIpsum.count).randomElement() ?? 123))
+        let stringToCut = [ randomXML, randomString, randomJson ].randomElement() ?? randomString
+        let from = (0 ..< stringToCut.count).randomElement() ?? 42
+        let to = from + ((0 ..< stringToCut.count - from).randomElement() ?? 239)
+        messageTextField.text = String(stringToCut.prefix(from + to).suffix(to - from))
     }
 
     // MARK: - UITextFieldDelegate
@@ -108,6 +111,15 @@ class SingleLogViewController: UIViewController, UITextFieldDelegate {
         } else {
             return true
         }
+    }
+
+    // MARK: - Data Generation
+
+    private var randomString: String {
+        let indexes: Range<Int> = (0 ..< (4 + ((0 ..< 5).randomElement() ?? 0)))
+        return indexes
+            .map { _ in "\("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<>\"\\".randomElement() ?? "_")" }
+            .joined(separator: "")
     }
 
     // swiftlint:disable line_length
@@ -143,6 +155,314 @@ class SingleLogViewController: UIViewController, UITextFieldDelegate {
         Tortor dignissim convallis aenean et. Habitant morbi tristique senectus et netus et malesuada fames ac. Maecenas volutpat blandit aliquam etiam. Non blandit massa enim nec dui. Ultricies tristique nulla aliquet enim tortor at auctor. Ornare arcu odio ut sem. Tellus mauris a diam maecenas. Nisi scelerisque eu ultrices vitae auctor eu. Vitae nunc sed velit dignissim sodales ut. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Eget mi proin sed libero enim sed faucibus.
         Proin nibh nisl condimentum id venenatis a. Blandit cursus risus at ultrices mi. Est sit amet facilisis magna etiam tempor orci eu. Lobortis mattis aliquam faucibus purus in massa tempor nec. Quam pellentesque nec nam aliquam. Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Diam vulputate ut pharetra sit amet aliquam id. Morbi tristique senectus et netus et malesuada fames ac. At risus viverra adipiscing at in. Sed arcu non odio euismod lacinia at quis risus. Feugiat in ante metus dictum at. Dignissim cras tincidunt lobortis feugiat vivamus at augue. Pellentesque habitant morbi tristique senectus et netus et malesuada. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Magna ac placerat vestibulum lectus mauris ultrices eros.
         Tempor nec feugiat nisl pretium fusce id velit ut. Risus nullam eget felis eget nunc lobortis mattis aliquam. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Malesuada fames ac turpis egestas sed tempus. Nunc aliquet bibendum enim facilisis gravida neque. Tristique sollicitudin nibh sit amet. Praesent tristique magna sit amet purus gravida quis blandit. Elementum nisi quis eleifend quam adipiscing vitae. Vehicula ipsum a arcu cursus vitae. Quam vulputate dignissim suspendisse in est. Mauris ultrices eros in cursus turpis massa tincidunt dui ut. Vestibulum morbi blandit cursus risus at ultrices mi tempus.
+        """
+
+    private let randomXML: String =
+        """
+        <root>
+          <mental>exact</mental>
+          <willing>1030631311</willing>
+          <broke>active</broke>
+          <garden>heat</garden>
+          <automobile>
+            <speak>
+              <women>flag</women>
+              <twelve>-1499016207</twelve>
+              <alive>
+                <law>1450036819.1712232</law>
+                <power>-1152082302.1965446</power>
+                <some>2001261389.8000946</some>
+                <same>-136018944.61708713</same>
+                <tie>259961729.98053646</tie>
+                <fox>-1451021263</fox>
+                <bottom>
+                  <sing>gradually</sing>
+                  <differ>1850847391.34653</differ>
+                  <charge>428647682</charge>
+                  <remove>agree</remove>
+                  <dirt>identity</dirt>
+                  <level>she</level>
+                  <mean>1963363397.0954678</mean>
+                  <equator>
+                    <seed>-391099188</seed>
+                    <tomorrow>1190189488</tomorrow>
+                    <chain>-1401049844</chain>
+                    <press>
+                      <straw>1747388758.8706017</straw>
+                      <vegetable>-1964655054.5100195</vegetable>
+                      <upper>
+                        <fish>cast</fish>
+                        <rise>489784044</rise>
+                        <claws>-158353059</claws>
+                        <atmosphere>characteristic</atmosphere>
+                        <since>-481713051</since>
+                        <crew>pick</crew>
+                        <start>organization</start>
+                        <tide>cotton</tide>
+                      </upper>
+                      <swung>card</swung>
+                      <shade>92663798</shade>
+                      <magic>dirty</magic>
+                      <birthday>physical</birthday>
+                      <if>shout</if>
+                    </press>
+                    <hope>-1655525653</hope>
+                    <grown>list</grown>
+                    <easily>-1795148238.786106</easily>
+                    <pine>appropriate</pine>
+                  </equator>
+                </bottom>
+                <sea>-1615055345</sea>
+              </alive>
+              <research>627255588</research>
+              <smoke>share</smoke>
+              <long>309874475.6211901</long>
+              <prove>able</prove>
+              <century>-193322380</century>
+            </speak>
+            <hat>-768788624</hat>
+            <lie>research</lie>
+            <syllable>1063012976.8054159</syllable>
+            <clear>-276151689</clear>
+            <question>666440401</question>
+            <cold>-984349792.9298351</cold>
+            <screen>-2144848108</screen>
+          </automobile>
+          <dollar>672692452</dollar>
+          <frighten>-2024896422</frighten>
+          <rays>road</rays>
+        </root>
+        """
+
+    private let randomJson: String =
+        """
+        [
+          {
+            "_id": "5ea9dadc946477a13920ae70",
+            "index": 0,
+            "guid": "5915f7a8-4b85-4a3e-83fb-ebda6a8567fe",
+            "isActive": true,
+            "balance": "$1,502.26",
+            "picture": "http://placehold.it/32x32",
+            "age": 27,
+            "eyeColor": "brown",
+            "name": "Mccormick Brooks",
+            "gender": "male",
+            "company": "INCUBUS",
+            "email": "mccormickbrooks@incubus.com",
+            "phone": "+1 (807) 595-3418",
+            "address": "984 Times Placez, National, Nevada, 7121",
+            "about": "Et consectetur id duis deserunt adipisicing adipisicing veniam do anim do. Nisi id et cillum aliqua incididunt dolor aliquip. Adipisicing incididunt consequat aliqua culpa ullamco id. Amet ex aliqua labore do sunt nulla. Incididunt enim excepteur ea officia tempor Lorem consequat laborum fugiat id magna nostrud. Officia nostrud excepteur ea quis tempor ex deserunt. Do ipsum sint tempor anim ullamco mollit nisi excepteur.\\r\\n",
+            "registered": "2018-09-16T04:52:49 -03:00",
+            "latitude": 41.071108,
+            "longitude": 170.631068,
+            "tags": [
+              "sint",
+              "cillum",
+              "eiusmod",
+              "veniam",
+              "adipisicing",
+              "ad",
+              "voluptate"
+            ],
+            "friends": [
+              {
+                "id": 0,
+                "name": "Kirby Clark"
+              },
+              {
+                "id": 1,
+                "name": "Lamb Norman"
+              },
+              {
+                "id": 2,
+                "name": "Decker Hutchinson"
+              }
+            ],
+            "greeting": "Hello, Mccormick Brooks! You have 2 unread messages.",
+            "favoriteFruit": "banana"
+          },
+          {
+            "_id": "5ea9dadcd259b71b12afad5c",
+            "index": 1,
+            "guid": "5d903910-a4c7-4eb8-9e56-10481f22cfe2",
+            "isActive": false,
+            "balance": "$3,421.87",
+            "picture": "http://placehold.it/32x32",
+            "age": 39,
+            "eyeColor": "blue",
+            "name": "Shelby Cummings",
+            "gender": "female",
+            "company": "HINWAY",
+            "email": "shelbycummings@hinway.com",
+            "phone": "+1 (833) 434-2413",
+            "address": "150 Cyrus Avenue, Sabillasville, Idaho, 3033",
+            "about": "Amet consectetur incididunt ut id et consectetur exercitation dolore ullamco nisi laboris. Cupidatat elit aliquip elit et occaecat aute pariatur aliquip laboris mollit consectetur officia Lorem laboris. Id elit qui veniam do aliquip labore Lorem cupidatat consequat. Anim tempor sint ipsum irure velit fugiat esse officia ullamco.\\r\\n",
+            "registered": "2014-12-30T06:00:26 -03:00",
+            "latitude": 17.928786,
+            "longitude": 113.966258,
+            "tags": [
+              "excepteur",
+              "sit",
+              "non",
+              "reprehenderit",
+              "pariatur",
+              "culpa",
+              "consectetur"
+            ],
+            "friends": [
+              {
+                "id": 0,
+                "name": "Rosa Gould"
+              },
+              {
+                "id": 1,
+                "name": "Allison Schneider"
+              },
+              {
+                "id": 2,
+                "name": "Petersen Hughes"
+              }
+            ],
+            "greeting": "Hello, Shelby Cummings! You have 4 unread messages.",
+            "favoriteFruit": "banana"
+          },
+          {
+            "_id": "5ea9dadce76ff454c74ff98c",
+            "index": 2,
+            "guid": "3afcae07-b92f-4368-aa09-58a3f854fd42",
+            "isActive": false,
+            "balance": "$1,848.64",
+            "picture": "http://placehold.it/32x32",
+            "age": 22,
+            "eyeColor": "blue",
+            "name": "Mathis Curtis",
+            "gender": "male",
+            "company": "XURBAN",
+            "email": "mathiscurtis@xurban.com",
+            "phone": "+1 (904) 534-2732",
+            "address": "327 Baycliff Terrace, Worcester, North Carolina, 485",
+            "about": "Eu excepteur ut exercitation tempor quis culpa et sit. Esse sunt veniam proident id eu veniam mollit esse. Ad Lorem qui in Lorem eiusmod est Lorem consequat elit. Magna irure aliquip fugiat adipisicing fugiat magna velit.\\r\\n",
+            "registered": "2014-09-28T02:58:15 -04:00",
+            "latitude": -62.373219,
+            "longitude": 165.902762,
+            "tags": [
+              "cupidatat",
+              "officia",
+              "voluptate",
+              "Lorem",
+              "dolor",
+              "irure",
+              "id"
+            ],
+            "friends": [
+              {
+                "id": 0,
+                "name": "Denise Cameron"
+              },
+              {
+                "id": 1,
+                "name": "Priscilla Hebert"
+              },
+              {
+                "id": 2,
+                "name": "Ward Bass"
+              }
+            ],
+            "greeting": "Hello, Mathis Curtis! You have 5 unread messages.",
+            "favoriteFruit": "banana"
+          },
+          {
+            "_id": "5ea9dadce73e544870fe870d",
+            "index": 3,
+            "guid": "3e2138ed-1789-4903-b0c3-0f814adfac60",
+            "isActive": true,
+            "balance": "$2,306.30",
+            "picture": "http://placehold.it/32x32",
+            "age": 30,
+            "eyeColor": "brown",
+            "name": "Shepherd Solis",
+            "gender": "male",
+            "company": "GYNKO",
+            "email": "shepherdsolis@gynko.com",
+            "phone": "+1 (825) 538-2116",
+            "address": "716 Cheever Place, Greenock, New York, 2440",
+            "about": "Cillum minim Lorem elit non laborum id irure dolore anim. Magna excepteur Lorem est minim. Ipsum consequat velit minim sunt non proident. Sunt fugiat anim dolore irure occaecat voluptate aliqua ad magna eiusmod ea. Dolore labore mollit eiusmod magna ex tempor nostrud eiusmod mollit sunt ad elit. Irure aute dolore veniam et irure.\\r\\n",
+            "registered": "2016-02-19T06:03:55 -03:00",
+            "latitude": 7.140264,
+            "longitude": 5.396599,
+            "tags": [
+              "exercitation",
+              "aliquip",
+              "sit",
+              "et",
+              "culpa",
+              "elit",
+              "sit"
+            ],
+            "friends": [
+              {
+                "id": 0,
+                "name": "Whitehead Wolf"
+              },
+              {
+                "id": 1,
+                "name": "Ilene Salinas"
+              },
+              {
+                "id": 2,
+                "name": "Francisca Mcneil"
+              }
+            ],
+            "greeting": "Hello, Shepherd Solis! You have 10 unread messages.",
+            "favoriteFruit": "strawberry"
+          },
+          {
+            "_id": "5ea9dadcd232444404f2d527",
+            "index": 4,
+            "guid": "f933b58b-8852-4614-8370-7e50a21599ec",
+            "isActive": true,
+            "balance": "$1,832.80",
+            "picture": "http://placehold.it/32x32",
+            "age": 40,
+            "eyeColor": "green",
+            "name": "Cecilia Stephens",
+            "gender": "female",
+            "company": "OPPORTECH",
+            "email": "ceciliastephens@opportech.com",
+            "phone": "+1 (817) 456-2502",
+            "address": "681 Tapscott Street, Townsend, Virginia, 2588",
+            "about": "Ea magna dolor deserunt in minim eiusmod esse aliquip esse sunt veniam aliquip voluptate. Amet eu esse culpa in cupidatat occaecat officia dolor do ullamco. Mollit excepteur veniam minim consectetur nostrud reprehenderit consectetur exercitation aute consectetur consectetur commodo consequat. Velit enim amet fugiat reprehenderit. Nostrud fugiat in est do duis anim quis amet qui qui mollit non dolor veniam.\\r\\n",
+            "registered": "2014-12-29T10:57:40 -03:00",
+            "latitude": -63.444416,
+            "longitude": -151.414767,
+            "tags": [
+              "ullamco",
+              "amet",
+              "aliqua",
+              "do",
+              "eu",
+              "pariatur",
+              "deserunt"
+            ],
+            "friends": [
+              {
+                "id": 0,
+                "name": "Chelsea Yates"
+              },
+              {
+                "id": 1,
+                "name": "Lawson Harper"
+              },
+              {
+                "id": 2,
+                "name": "Moran Obrien"
+              }
+            ],
+            "greeting": "Hello, Cecilia Stephens! You have 4 unread messages.",
+            "favoriteFruit": "apple"
+          }
+        ]
         """
     // swiftlint:enable line_length
 }
