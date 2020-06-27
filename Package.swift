8060c7eabd72ccedbad80074be51f4af4ffe6165
Alex Babaev <alex@devpocket.com>
2020-06-27 22:34:30 +0300

    Moved things around. Preparing to split Robologs to local/remote frameworks.

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ let package = Package(
     name: "Robologs",
     platforms: [ .iOS(.v11), .macOS(.v10_15) ],
     products: [
-        .library(name: "Robologs", type: .static, targets: [ "Robologs" ])
+        .library(name: "Robologs", type: .static, targets: [ "Robologs" ]),
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.8.0")
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.8.0"),
     ],
     targets: [
-        .target(name: "Robologs", dependencies: [ "SwiftProtobuf" ], path: "Sources")
+        .target(name: "Robologs", dependencies: [ "SwiftProtobuf" ], path: "", sources: [ "Sources.Robologs", "Sources.Robologs.Remote" ]),
     ],
     swiftLanguageVersions: [.v5]
 )
