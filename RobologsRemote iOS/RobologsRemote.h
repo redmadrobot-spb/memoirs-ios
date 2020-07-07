4c7fb5898c4668a5f0f7daae4a161d36fa5234d4
Alex Babaev <alex@devpocket.com>
2020-07-07 18:23:32 +0300

    Added targets for RobologsRemote

---
+++ RobologsRemote iOS/RobologsRemote.h
@@ -0,0 +1,19 @@
+//
+//  RobologsRemote.h
+//  RobologsRemote
+//
+//  Created by Alex Babaev on 07.07.2020.
+//  Copyright © 2020 Redmadrobot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RobologsRemote.
+FOUNDATION_EXPORT double RobologsRemoteVersionNumber;
+
+//! Project version string for RobologsRemote.
+FOUNDATION_EXPORT const unsigned char RobologsRemoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RobologsRemote/PublicHeader.h>
+
+
