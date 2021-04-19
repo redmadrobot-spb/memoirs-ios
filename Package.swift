27041d0484f7eab2071f7c3a1df1f52cb09a05f5
Alex Babaev <alex@devpocket.com>
2021-04-19 15:16:23 +0300

    Removed everything except spm

--- Package.swift
+++ Package.swift
@@ -8,13 +8,20 @@ let package = Package(
     products: [
         .library(name: "Robologs", type: .static, targets: [ "Robologs" ]),
         .library(name: "RobologsRemote", type: .static, targets: [ "RobologsRemote" ]),
+        .executable(name: "BonjourClientTest", targets: [ "BonjourClientTest" ])
     ],
     dependencies: [
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.8.0"),
     ],
     targets: [
+        .target(name: "BonjourClientTest", dependencies: [ "RobologsRemote" ], path: "Sources.BonjourClientTest"),
         .target(name: "Robologs", dependencies: [], path: "Sources.Robologs"),
-        .target(name: "RobologsRemote", dependencies: [ "Robologs", "SwiftProtobuf" ], path: "Sources.Robologs.Remote"),
+        .target(
+            name: "RobologsRemote",
+            dependencies: [ "Robologs", "SwiftProtobuf" ],
+            path: "Sources.Robologs.Remote",
+            exclude: [ "Transports/ProtoHttpRemoteLoggerTransport/proto/backend.proto" ]
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
