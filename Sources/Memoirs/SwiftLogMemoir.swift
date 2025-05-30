7c2ec7780b4785adeedb507f8f342a5799bfa3af
Alex Babaev <a.babaev@redmadrobot.com>
2025-05-30 16:39:22 -0400

    Fixed Swift-Log memoir

--- Sources/Memoirs/SwiftLogMemoir.swift
+++ Sources/Memoirs/SwiftLogMemoir.swift
@@ -6,11 +6,13 @@
 // Copyright © 2024 Alexander Babaev. All rights reserved.
 //
 
+import Foundation
+
 #if canImport(Logging)
 
 import Logging
 
-public final class SwiftLogMemoir: Memoir {
+public final class SwiftLogMemoir: Memoir, @unchecked Sendable {
     private let swiftLoggerMapper: ([Tracer]) -> Logging.Logger?
     private let defaultLogger: Logging.Logger = .init(label: "default")
 
@@ -22,10 +24,11 @@ public final class SwiftLogMemoir: Memoir {
         hideSensitiveValues: Bool,
         markers: Output.Markers = .init(),
         interceptor: (@Sendable (String) -> Void)? = nil,
-        swiftLoggerMapper: @escaping ([Tracer]) -> Logging.Logger?
+        swiftLoggerMapper: @escaping ([Tracer]) -> Logging.Logger?,
+        tracerFilter: @escaping @Sendable (Tracer) -> Bool = PrintMemoir.defaultTracerFilter,
     ) {
         self.hideSensitiveValues = hideSensitiveValues
-        self.loggerMapper = swiftLoggerMapper
+        self.swiftLoggerMapper = swiftLoggerMapper
         self.interceptor = interceptor
         output = Output(
             markers: markers,
@@ -35,7 +38,6 @@ public final class SwiftLogMemoir: Memoir {
         )
     }
 
-    @inlinable
     public func append(
         _ item: MemoirItem,
         message: @autoclosure () throws -> SafeString,
@@ -44,47 +46,50 @@ public final class SwiftLogMemoir: Memoir {
         timeIntervalSinceReferenceDate: TimeInterval,
         file: String, function: String, line: UInt
     ) rethrows {
-        let loggingType: Logging.Logger.Level
+        let codePosition = output.codePosition(file: file, function: function, line: line)
+        let description: String
+
+        let loggingLevel: Logging.Logger.Level
         switch item {
             case .log(let level):
                 description = try output.logString(
                     date: nil, level: level, message: message, tracers: tracers, meta: meta, codePosition: codePosition
                 ).joined(separator: " ")
                 switch level {
-                    case .critical: loggingType = .critical
-                    case .error: loggingType = .error
-                    case .warning: loggingType = .warning
-                    case .info: loggingType = .info
-                    case .debug: loggingType = .debug
-                    case .verbose: loggingType = .trace
+                    case .critical: loggingLevel = .critical
+                    case .error: loggingLevel = .error
+                    case .warning: loggingLevel = .warning
+                    case .info: loggingLevel = .info
+                    case .debug: loggingLevel = .debug
+                    case .verbose: loggingLevel = .trace
                 }
             case .event(let name):
                 description = output.eventString(
                     date: nil, name: name, tracers: tracers, meta: meta, codePosition: codePosition
                 ).joined(separator: " ")
-                loggingType = .info
+                loggingLevel = .info
             case .tracer(let tracer, false):
                 description = output.tracerString(
                     date: nil, tracer: tracer, tracers: tracers, meta: meta, codePosition: codePosition
                 ).joined(separator: " ")
-                loggingType = .info
+                loggingLevel = .info
             case .tracer(let tracer, true):
                 description = output.tracerEndString(
                     date: nil, tracer: tracer, tracers: tracers, meta: meta, codePosition: codePosition
                 ).joined(separator: " ")
-                loggingType = .info
+                loggingLevel = .info
             case .measurement(let name, let value):
                 description = output.measurementString(
                     date: nil, name: name, value: value, tracers: tracers, meta: meta, codePosition: codePosition
                 ).joined(separator: " ")
-                loggingType = .info
+                loggingLevel = .info
         }
 
         let logger = swiftLoggerMapper(tracers) ?? defaultLogger
         logger.log(
             level: loggingLevel,
-            message: description,
-            metadata: meta().mapValues { $0.string(hideSensitiveValues: hideSensitiveValues) },
+            "\(description)",
+            metadata: meta()?.mapValues { .stringConvertible($0.string(hideSensitiveValues: hideSensitiveValues)) },
             source: nil, // TODO:
             file: file, function: function, line: line
         )
